856928d215d51f1fcd5ccefd8235844ac41db3f6
Bobby2Kills <e.rob93@yahoo.co.uk>
2023-06-08 13:20:28 +0100

    added-some-comments

--- src/main/java/Game/Service/ApiService.java
+++ src/main/java/Game/Service/ApiService.java
@@ -11,13 +11,17 @@ import org.springframework.web.client.RestTemplate;
 public class ApiService {
 
     public void savePlayerName(String playerName) {
+        // defines 'url' as the endpoint + the playerName entered
         String url = "http://localhost:8102/save?playerName=" + playerName;
 
+        // Gives the playerName in JSON format
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
 
+        // Creates an instance of the HttpEntity using the header created
         HttpEntity<String> requestEntity = new HttpEntity<>(headers);
 
+        // RestTemplate sends the POSt request, takes the URL and HTTP, Request Entity and gives the response type.
         RestTemplate restTemplate = new RestTemplate();
         ResponseEntity<String> response = restTemplate.exchange(
                 url,
@@ -26,6 +30,7 @@ public class ApiService {
                 String.class
         );
 
+        // Checks the response was actually successful
         if (response.getStatusCode().is2xxSuccessful()) {
             System.out.println("Player name saved to the database.");
         }
